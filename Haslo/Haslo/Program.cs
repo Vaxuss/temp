4bb2d033004e57d166e5bbb033621be6e465503c
Vaxuss <a.derybowski@wp.pl>
2022-07-23 21:19:48 +0200

    adminihaslo

---
+++ Haslo/Haslo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haslo
+{
+    internal class Program
+    {
+        static int proby = 3;
+        static int Main()
+        {
+            Console.WriteLine("Podaj nazwe urzytkownika: ");
+            string user = Console.ReadLine();
+            Console.WriteLine("Podaj Haslo: ");
+            string pass = Console.ReadLine();
+
+            if (pass != "haslo" && proby > 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Złe hasło! pozostałe proby: " + --proby);
+                return Main();
+            }
+            else if (pass == "haslo")
+            {
+                if(user == "admin")
+                {
+                    Console.WriteLine("Admin? serio?");
+                    Console.ReadLine();
+                }
+            }
+            else if (proby == 0) Environment.Exit(0);
+            return 0;
+        }
+    }
+}
